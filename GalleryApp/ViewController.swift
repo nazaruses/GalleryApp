1391d6f1fd7751ec0a6c6b731415df17b2cff966
Nazar <thenazardremach@gmail.com>
2023-08-20 16:36:21 +0200

    Initial Commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Nazar Dremach on 20/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
